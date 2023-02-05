6c0c06c4a84ca2a4ddc775ce68d70fcc6c2dd97c
anvar-muzaffarli <prof.anvar2013@gmail.com>
2023-02-05 16:03:42 +0400

    Navbar yaradildi ve useRef oyredildi

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,96 @@
+import React, { useRef } from 'react'
+import './Navbar.css'
+import brendinLogosu from '../assets/images/logo.svg'
+
+import { AiOutlineBars } from "react-icons/ai"
+import {GiCrossedSwords} from 'react-icons/gi'
+
+
+
+const Navbar = () => {
+
+  const overlayDivininUnvaniRef = useRef()
+  // console.log(overlayDivininUnvaniRef.current)
+
+  const openOverlayMenu = (e) => {
+    const kliklenenElement = e.target
+
+    console.log(kliklenenElement)
+
+    if(kliklenenElement.classList.contains('bars-icon')) {
+      overlayDivininUnvaniRef.current.classList.add('aktiv')
+    }
+   
+  }
+
+  const closeOverlayMenu = (e) => {
+    const kliklenenElement = e.target
+
+    console.log(kliklenenElement)
+
+    if(kliklenenElement.classList.contains('cross-icon')) {
+      overlayDivininUnvaniRef.current.classList.remove('aktiv')
+    }
+  }
+  
+    {/* JSX deyishenler {} icerisinde yazilir */}
+  return (
+
+    <>
+
+
+    <div className="overlay" ref={overlayDivininUnvaniRef}>
+      <GiCrossedSwords className='cross-icon' onClick={closeOverlayMenu}  />
+
+      <div className="nav-links">
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="#">Service</a>
+      <a href="#">Blog</a>
+      <a href="#">Contact</a>
+
+      
+
+      </div>
+    </div>
+
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#"><img src={brendinLogosu} alt="" /></a>
+    <button onClick={openOverlayMenu} className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+     <AiOutlineBars className='bars-icon'/>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link  text-white" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link text-white" href="#">About</a>
+        </li>
+
+        <li className="nav-item">
+          <a className="nav-link" href="#">Services</a>
+        </li>
+
+        <li className="nav-item">
+          <a className="nav-link" href="#">Blog</a>
+        </li>
+
+        <li className="nav-item">
+        <a className="nav-link" href="#">Contact</a>
+      </li>
+      
+       
+      </ul>
+     <button className="btn btn-primary">Sign In</button>
+    </div>
+  </div>
+</nav>
+
+    
+</>
+  )
+}
+
+export default Navbar
---
+++ src/components/demo.js
@@ -0,0 +1,33 @@
+telebe1= {
+    ad:"Necmeddin",
+    soyad:"Semed"
+}
+
+
+const telebeler= [
+    {
+        ad:"Necmeddin",
+        soyad:"Semed"
+    },
+    {
+        ad:"Valide",
+        soyad:"Ceferli"
+    },
+    {},
+    {}
+]
+
+telebeler[1].ad
+
+console.log(telebe1.ad)
+
+// adsiz funksiyalar
+// const sayHi = function (name) 
+//  "Salam" + name
+
+
+// function sayHi2 name {
+//     return "Salam" + name
+// }
+
+const sayHi3 = name =>  "Salam" + name
